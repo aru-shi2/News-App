4ee401015647d6f73f84f61034637251ef9a76c5
aru-shi2 <arushimishra2929@gmail.com>
2025-11-18 05:02:08 +0530

    add read more button

--- script.js
+++ script.js
@@ -17,14 +17,17 @@ function handleClick() {
       let el1=document.createElement("img")
       let el2=document.createElement("h1")
       let el3=document.createElement("p")
+      let btn=document.createElement("button")
 
     el1.src=e.urlToImage
     el2.textContent=e.title
     el3.textContent=e.description
+    btn.textContent="Read More"
 
     el.appendChild(el1)
     el.appendChild(el2)
     el.appendChild(el3)
+    el.appendChild(btn)
 
     parent.appendChild(el);
     });
@@ -35,7 +38,8 @@ function handleClick() {
   const data=async() => {
     let a=await fetch(`https://newsapi.org/v2/everything?q=news&apiKey=${api_key}`)
     const res=await a.json()
-       
+    console.log(res)
+
     const parent=document.querySelector("ul")
 
     res.articles.forEach(e => {
@@ -43,16 +47,24 @@ function handleClick() {
       let el1=document.createElement("img")
       let el2=document.createElement("h1")
       let el3=document.createElement("p")
+      let btn=document.createElement("button")
+      
 
     el1.src=e.urlToImage
     el2.textContent=e.title
     el3.textContent=e.description
+    btn.textContent="Read More"
 
     el.appendChild(el1)
     el.appendChild(el2)
     el.appendChild(el3)
+    el.appendChild(btn)
 
     parent.appendChild(el);
+
+    btn.addEventListener("click",()=>{
+      window.open(e.url,"_blank")
+    })
     });
     
   }
