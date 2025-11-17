fbfc0976ed3b7840285e9405578470cf01729e2b
aru-shi2 <arushimishra2929@gmail.com>
2025-11-17 21:55:32 +0530

    add search functionality

--- script.js
+++ script.js
@@ -2,18 +2,47 @@ import {api_key} from "./config.js";
 
 document.getElementById("click").addEventListener("click",handleClick);
 
+function handleClick() {
+    const search=async () => {
+      let txt=document.querySelector("input").value
+      let b=await fetch(`https://newsapi.org/v2/everything?q=${txt}&apiKey=${api_key}`)
+      const result=await b.json()
+
+      document.querySelector("ul").innerHTML=""
+
+      const parent=document.querySelector("ul")
+
+    result.articles.forEach(e => {
+      let el=document.createElement("li")
+      let el1=document.createElement("img")
+      let el2=document.createElement("h1")
+      let el3=document.createElement("p")
+
+    el1.src=e.urlToImage
+    el2.textContent=e.title
+    el3.textContent=e.description
+
+    el.appendChild(el1)
+    el.appendChild(el2)
+    el.appendChild(el3)
+
+    parent.appendChild(el);
+    });
+}
+     search()
+  }
+
   const data=async() => {
     let a=await fetch(`https://newsapi.org/v2/everything?q=news&apiKey=${api_key}`)
     const res=await a.json()
-    console.log(res)
-
+       
     const parent=document.querySelector("ul")
 
     res.articles.forEach(e => {
-      const el=document.createElement("li")
-      const el1=document.createElement("img")
-      const el2=document.createElement("h1")
-      const el3=document.createElement("p")
+      let el=document.createElement("li")
+      let el1=document.createElement("img")
+      let el2=document.createElement("h1")
+      let el3=document.createElement("p")
 
     el1.src=e.urlToImage
     el2.textContent=e.title
@@ -28,7 +57,3 @@ document.getElementById("click").addEventListener("click",handleClick);
     
   }
   data()
-
-  function handleClick() {
-    fg
-  }
